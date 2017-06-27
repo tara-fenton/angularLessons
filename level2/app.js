3df833085433e6eb2ed6488d99218c2c22426b62
Tara Fenton <tarafenton21@gmail.com>
2017-06-27 13:47:33 -0400

    Using Gallery Controller

--- level2/app.js
+++ level2/app.js
@@ -1,14 +1,6 @@
 (function() {
   var app = angular.module('gemStore', []);
 
-  app.controller('GalleryController', function(){
-    this.current =0;
-    
-    this.setCurrent = function(newGallery){
-      this.current = newGallery || 0;
-    };
-  });
-
   app.controller('StoreController', function(){
     this.products = gems;
   });
@@ -22,6 +14,13 @@
 
     this.isSet = function(tabName){
       return this.tab === tabName;
+    };
+  });
+
+  app.controller('GalleryController', function(){
+    this.current = 0;
+    this.setCurrent = function(newGallery){
+      this.current = newGallery || 0;
     };
   });
 
